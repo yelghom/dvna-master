f6db1b509967b7243a4d85f01a3ee01d217beb2a
“Yousra <yousra.elghomari@gmail.comgit config --global user.email yousra.elghomari@gmail.com>
2024-10-06 22:31:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Random Web APP
+
+### Using Official Docker Image
+
+TODO HERE
+
+### Manual Setup
+
+Clone the repository
+
+```bash
+git clone https://github.com/appsecco/dvna
+cd dvna
+```
+
+Install Dependencies
+
+```bash
+npm install
+```
+
+Start the application
+
+```bash
+npm start
+```
+Access the application at [http://localhost:9090](http://localhost:9090)
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var passport = require('passport')
+var session = require('express-session')
+var ejs = require('ejs')
+var morgan = require('morgan')
+const fileUpload = require('express-fileupload');
+
+//Initialize Express
+var app = express()
+require('./core/passport')(passport)
+app.use(express.static('public'))
+app.set('view engine','ejs')
+app.use(morgan('tiny'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(fileUpload());
+
+// Enable for Reverse proxy support
+// app.set('trust proxy', 1) 
+
+// Intialize Session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+// Initialize Passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Initialize express-flash
+app.use(require('express-flash')());
+
+// Routing
+app.use('/app',require('./routes/app')())
+app.use('/',require('./routes/main')(passport))
+
+// Start Server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
