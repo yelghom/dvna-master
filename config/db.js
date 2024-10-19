7da1a7ced20735886a79df161fd17a7294ab1953
Yousra El Ghomari <yelghom@users.noreply.github.com>
2024-10-18 23:10:55 -0400

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,26 @@
+require("dotenv").config(); 
+
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+  }
+);
+
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection to PostgreSQL has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+module.exports = sequelize;
