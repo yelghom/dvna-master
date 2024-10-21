def370b1974e6e524b0c31e9949c71c5c88430a5
Ayah Farhat <97855138+AyahFarhat@users.noreply.github.com>
2024-10-20 21:43:52 -0400

    adding log

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 require("dotenv").config(); 
 
 const { Sequelize } = require("sequelize");
-
+console.log("db.js")
+console.log("DATABASE_URL", process.env.DATABASE_URL);
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
 });
